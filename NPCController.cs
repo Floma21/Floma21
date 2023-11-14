e667f720fa90087f59aefee05a4ff865464e01d5
Floma21 <150169407+Floma21@users.noreply.github.com>
2023-11-14 19:44:58 +0300

    Add files via upload

---
+++ NPCController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    // Здоровье NPC
+    public int health = 5;
+    // уровень
+    public int level = 1;
+    // Скорость
+    public float speed = 1.5f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
